74640089a93dc0f7d6966027ef236097c827f89e
dlcdev1 <dlclegal@hotmail.com>
2018-12-05 09:19:54 -0200

    subindo arquivos java

---
+++ basico-java-oo/src/br/com/aula13/labs/exercicio8.java
@@ -0,0 +1,27 @@
+package br.com.aula13.labs;
+
+import java.util.Scanner;
+
+public class exercicio8 {
+
+	public static void main(String[] args) {
+		// Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês
+		//Calcule e mostre o total do seu salario no feferido mês.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("===========Salario total mês============");
+		System.out.println("Valor recebido por hora: ");
+		double valorHoras =  scan.nextDouble();
+		
+		System.out.println("Qtd horas trabalhadas:");
+		double horasTrab = scan.nextDouble();
+		
+		double resultado = valorHoras * horasTrab;
+		
+		System.out.println("O seu salário será de:" + resultado);
+		
+
+	}
+
+}
---
+++ basico-java-oo/src/br/com/aula15/exercicio1.java
@@ -0,0 +1,27 @@
+package br.com.aula15;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		//https://www.slideshare.net/loianeg/curso-java-basico-exercicios-aulas-14-15
+		// Faça um programa que peça dois numeros e imprima o maior deles.
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro numero:");
+		int number1 = scan.nextInt();
+		
+		System.out.println("Informe o segundo numero:");
+		int number2 = scan.nextInt();
+		
+		if(number1 > number2 ) {
+			System.out.println("O maior numero :" + number1 );
+			
+		} else {
+			System.out.println("O maior nmero :" + number2);
+		}
+
+	}
+
+}
