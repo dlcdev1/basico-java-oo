fe4ae52d8754260baa988452504edac32e964dcd
David Leandro Correia <1152126@sga.pucminas.br>
2018-12-05 20:57:59 -0200

    Exercicio após o 10

---
+++ basico-java-oo/src/br/com/aula15/exercicio10.java
@@ -0,0 +1,15 @@
+package br.com.aula15;
+
+import java.util.Scanner;
+
+public class exercicio10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+
+}
