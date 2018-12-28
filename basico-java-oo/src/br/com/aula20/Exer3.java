57b540d61707b126dabd1ce356d7e4d448d63551
David Leandro Correia <dlclegal@hotmail.com>
2018-12-27 23:08:27 -0200

    enviar para atuzlização

--- basico-java-oo/src/br/com/aula20/Exer3.java
+++ basico-java-oo/src/br/com/aula20/Exer3.java
@@ -9,14 +9,32 @@ public class Exer3 {
 		
 		int [][] matriz = new int [3][3];
 		int valor=0;
+		int numberP =0;
+		int numberI =0;
 		for (int i=0; i  < matriz.length; i++) {
 			for(int j=0; j < matriz[i].length; j++) {
-				System.out.println("Informe o valor da coluna " + i);
+				System.out.println("Informe o valor da coluna " + (i+1));
 				valor = scan.nextInt();
 				matriz[i][j] = valor;
 			}
 		}
+		for(int i=0; i < matriz.length; i++) { 
+			System.out.print("L" + (i+1) +" ");
+			for (int j =0; j< matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " " );
+				if(matriz[i][j] %2==0) {
+					numberP++;
+				} else {
+					numberI++;
+				}
+			}
+			System.out.println();
+			
 
+		}
+		System.out.println("Quantidade números Par: " + numberP);
+		System.out.println("Quantidade números Par: " + numberI);
+		
 	}
 
 }
