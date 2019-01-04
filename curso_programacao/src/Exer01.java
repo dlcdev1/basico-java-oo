a71ef1839d5ce5f6d8ba1c3e8b23b0436ca995ac
dlcdev1 <dlclegal@hotmail.com>
2019-01-04 18:59:58 -0200

    Subindo novos arquivos

---
+++ curso_programacao/src/Exer01.java
@@ -0,0 +1,35 @@
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("Office desk, which price is  $ %.2f%n", price2);
+		/*
+		 * %f = ponto flutuante
+		 * %d = inteiro
+		 * %s = texto
+		 * %n = quebra de linha
+		 * */
+		System.out.printf("Record: %d years old, code %d and gender: %s\n", age, code, gender);
+		System.out.println("Measue with eight decimal place: " + measure);
+		System.out.printf("Rouded (three decimal place): %.3f%n", measure);
+		System.out.printf("US decimal point: %.3f%n", measure);
+		
+		
+		
+
+	}
+
+}
--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,8 +1,30 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
+		double x = 10.35784;
+		
+		String nome = "Maria";
+		int idade = 31;
+		double renda =4000.0;
+		
+		
 		System.out.println("Ola mundo!");
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R& %.2f reais %n", nome, idade, renda);
+		
+		//Locale.setDefault(Locale.US);
+		
+		/*
+		 * %f = ponto flutuante
+		 * %d = inteiro
+		 * %s = texto
+		 * %n = quebra de linha
+		 * */
 
 	}
 
