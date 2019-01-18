f67c6f4672dcfd194d258e7393be4b95e3274e83
David Leandro Correia <1152126@sga.pucminas.br>
2019-01-17 22:37:36 -0200

    adicinando arquivos oo

---
+++ basico-java-oo/src/nivelamento/condicao_ternaria.java
@@ -0,0 +1,16 @@
+package nivelamento;
+
+public class condicao_ternaria {
+
+	public static void main(String[] args) {
+		// condição ternaria
+		double preco = 34.5;
+		double desconto = (preco < 20.0)? preco * 0.1 : preco * 0.05;
+		
+		System.out.println(desconto);
+				
+	}
+
+}
+
+
